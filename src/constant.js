2e36c4f8a68302423d281bcee2b147736fd43464
unknown <ziywang50@gmail.com>
2024-12-01 12:33:41 -0800

    social_ai

---
+++ src/constant.js
@@ -0,0 +1,9 @@
+export const TOKEN_KEY = "token";
+export const BASE_URL = "https://socialaione.wl.r.appspot.com";
+//export const BASE_URL = "https://socialai0812.uw.r.appspot.com";
+//export const BASE_URL = "https://socialai202406.uw.r.appspot.com";
+export const SEARCH_KEY = {
+  all: 0,
+  keyword: 1,
+  user: 2,
+};
